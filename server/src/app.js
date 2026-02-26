0271ca9e8f9d52648c3241144a7afa869eb1b36e
Pirathap <pirathap.dev@gmail.com>
2026-02-26 15:41:26 +0530

    Modified app.js according to finance component - added routes for finance component

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,10 @@ import cors from "cors";
 import authRoutesMarketing from "./routes/authRoutesMarketing.js";
 import { notFoundMarketing, errorHandlerMarketing } from "./middleware/errorMiddlewareMarketing.js";
 
+import financeRoutes from "./routes/financeRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import revenueRoutes from "./routes/revenueRoutes.js";
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +16,10 @@ app.use(cors());
 app.get("/", (req, res) => res.send("VentureAssist Backend Running..."));
 
 app.use("/api/marketing/auth", authRoutesMarketing);
+
+app.use("/api/finance", financeRoutes);
+app.use("/api/finance/expenses", expenseRoutes);
+app.use("/api/finance/revenue", revenueRoutes);
 
 app.use(notFoundMarketing);
 app.use(errorHandlerMarketing);
